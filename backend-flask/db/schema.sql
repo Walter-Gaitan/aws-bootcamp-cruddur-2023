640758e79f9ca258eed812b84f8a7670333fe3a9
Walter Gaitan <walterg.steven@gmail.com>
2023-03-18 20:04:28 +0000

    Add changes

--- backend-flask/db/schema.sql
+++ backend-flask/db/schema.sql
@@ -1,11 +1,10 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 DROP TABLE IF EXISTS public.users;
 DROP TABLE IF EXISTS public.activities;
 
+
 CREATE TABLE public.users (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  user_uuid UUID NOT NULL,
   display_name text,
   handle text,
   cognito_user_id text,
@@ -14,6 +13,7 @@ CREATE TABLE public.users (
 
 CREATE TABLE public.activities (
   uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+  user_uuid UUID NOT NULL,
   message text NOT NULL,
   replies_count integer DEFAULT 0,
   reposts_count integer DEFAULT 0,
