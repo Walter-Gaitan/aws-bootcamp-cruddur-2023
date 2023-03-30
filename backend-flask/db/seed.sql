a8174920ac252046957fe5a676563a111910a669
Walter Gaitan <walterg.steven@gmail.com>
2023-03-30 20:10:19 +0000

    PENDING: Token was not issued for this audience error

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -2,12 +2,15 @@
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
   ('Andrew Brown','andrew@exampro.co' , 'andrewbrown' ,'MOCK'),
-  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK');
+  ('Andrew Bayko','bayko@exampro.co' , 'bayko' ,'MOCK'),
+  ('Walter Gaitan','walterg.steven@gmail.com' ,'waltergsteven' ,'MOCK'),
+  ('Londo Mollari','lmollari@centari.com' ,'londo' ,'MOCK');
+
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid FROM public.users WHERE users.handle = 'waltergsteven' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
-  );
+  )
